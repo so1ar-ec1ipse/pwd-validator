86270d54e7e96af38c51beccaa69b8833fa07669
Corey Breshears <corey.l.breshears@outlook.com>
2024-01-15 11:42:48 -0500

    Validate password inputs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import PasswordInput from "./components/PasswordInput";
+import ErrorMessage from "./components/ErrorMessage";
+import {
+  hasMinSix,
+  hasAtLeastOneLower,
+  hasAtLeastOneUpper,
+  hasAtLeastOneDigit,
+  hasAtLeastOneSpecial,
+} from "./utils/validators";
+
+const validators = [
+  { validator: hasMinSix, errorMsg: "Password should have a min length of 6 characters" },
+  {
+    validator: hasAtLeastOneUpper,
+    errorMsg: "Password should have at least 1 uppercase character",
+  },
+  {
+    validator: hasAtLeastOneLower,
+    errorMsg: "Password should have at least 1 lowercase character",
+  },
+  { validator: hasAtLeastOneDigit, errorMsg: "Password should have at least 1 number" },
+  {
+    validator: hasAtLeastOneSpecial,
+    errorMsg: `Password should have at least 1 special character (!@#$%^&*()_-+={[}]|:;"'<,>.)' }]`,
+  },
+];
 
 function App() {
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [isPwdValid, setIsPwdValid] = useState(false);
+  const [errorMessages, setErrorMessages] = useState([]);
+
+  // Check if passwords match
+  const validatePassword = () => {
+    if (password !== confirmPassword) {
+      setIsPwdValid(false);
+      setErrorMessages(["Passwords do not match"]);
+      return;
+    }
+
+    const messages = [];
+
+    for (const { validator: isValid, errorMsg } of validators) {
+      if (!isValid(password)) messages.push(errorMsg);
+    }
+
+    if (messages.length) setIsPwdValid(false);
+    else setIsPwdValid(true);
+
+    setErrorMessages(messages);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    validatePassword();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <PasswordInput
+          label="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <PasswordInput
+          label="Confirm Password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+        <br />
+        <button type="submit" disabled={!password.length}>
+          Submit
+        </button>
+      </form>
+
+      {isPwdValid ? (
+        <div style={{ color: "green" }}>Password is valid!</div>
+      ) : (
+        errorMessages.map((msg) => <ErrorMessage message={msg} />)
+      )}
     </div>
   );
 }
